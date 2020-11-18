bcdb3d181a6c5efd0b5f31debb88272de88424d7
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-18 14:53:59 +0700

    remove redundant

--- src/features/Board/Auth/authAPI.ts
+++ src/features/Board/Auth/authAPI.ts
@@ -6,25 +6,11 @@ class AuthAPI {
     const response = await axiosClient.post(`${this.endPoint}/login`, { email, password });
     return response.data.response.access_token;
   }
-  signUp = async (userData: { name: string, email: string, password: string }) => {
+  signUp = async (userData: { name: string, email: string, password: string | null }) => {
     const { email, name, password } = userData;
     const response = await axiosClient.post(`${this.endPoint}/register`, { email, password, name });
     return response.data.response;
   }
-  // addCard = async (cardData: { name: string, idBoard: string, idList: string, pos: number }) => {
-  //   const { name, idBoard, idList, pos } = cardData;
-  //   const response = await axiosClient.post(this.endPoint, { name, idBoard, idList, pos });
-  //   return response.data.response.card;
-  // }
-  // updateCard = async (idCard: string, cardData: { name: string }) => {
-  //   const { name } = cardData;
-  //   const response = await axiosClient.patch(`${this.endPoint}/${idCard}`, { name });
-  //   return response.data.response.card;
-  // }
-  // deleteCard = async (idCard: string) => {
-  //   const response = await axiosClient.delete(`${this.endPoint}/${idCard}`);
-  //   // return response.data.response.card;
-  // }
 
 }
 
