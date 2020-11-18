ae4fe858e3f55c70136f30feb7c593cc93ef64f6
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-18 11:49:05 +0700

    add sign up page from template

---
+++ src/features/Board/Auth/SignUp.tsx
@@ -0,0 +1,105 @@
+import { Avatar, Button, Container, CssBaseline, Grid, makeStyles, TextField, Typography } from "@material-ui/core";
+import { LockOutlined } from '@material-ui/icons';
+import React from "react";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  link: {
+    '& a': {
+      textDecoration: 'none',
+      color: 'inherit'
+    }
+  }
+}));
+
+//template from // template from https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-up
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item className={classes.link}>
+              <Typography variant='body2' noWrap>
+                <Link to='/login'>
+                  Already have an account? Log in
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
