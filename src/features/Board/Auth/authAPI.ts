717e9963e33e0997f3e20de9dd9ad1c88932057c
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-18 11:49:21 +0700

    intial auth API

---
+++ src/features/Board/Auth/authAPI.ts
@@ -0,0 +1,27 @@
+import axiosClient from "../../../api/axiosClient";
+
+class AuthAPI {
+  endPoint = '/auth';
+
+  // addCard = async (cardData: { name: string, idBoard: string, idList: string, pos: number }) => {
+  //   const { name, idBoard, idList, pos } = cardData;
+  //   const response = await axiosClient.post(this.endPoint, { name, idBoard, idList, pos });
+  //   return response.data.response.card;
+  // }
+  // updateCard = async (idCard: string, cardData: { name: string }) => {
+  //   const { name } = cardData;
+  //   const response = await axiosClient.patch(`${this.endPoint}/${idCard}`, { name });
+  //   return response.data.response.card;
+  // }
+  // deleteCard = async (idCard: string) => {
+  //   const response = await axiosClient.delete(`${this.endPoint}/${idCard}`);
+  //   // return response.data.response.card;
+  // }
+
+}
+
+const authAPI = new AuthAPI();
+
+export {
+  authAPI
+};
